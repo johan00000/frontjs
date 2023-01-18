4a6ab26af79b62b6e3ad24f36b6aa521a8c53df2
johan orozco <johanorozco496@gmail.com>
2023-01-18 15:13:57 -0500

    actualizado hasta que sale solo se han estado fucionando en el inde

---
+++ index.js
@@ -0,0 +1,8 @@
+let email=document.querySelector('.navbar-email')
+let barra=document.querySelector('.desktop-menu')
+email.addEventListener('click',myCuenta)
+function myCuenta()
+{
+    barra.classList.toggle('cambio')
+
+}
