4a2dc9acf797c2d55cc80ceeb1df33fe1167955e
johan orozco <johanorozco496@gmail.com>
2023-01-18 20:29:17 -0500

    se volvio responsivo el menu de usuario para destok y movile utilizando media cueris para que se desaparesca al cambio de pantalla

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-let email=document.querySelector('.navbar-email')
-let barra=document.querySelector('.desktop-menu')
-email.addEventListener('click',myCuenta)
+//llamado de interaciones menu
+let email=document.querySelector('.navbar-email');
+let barra=document.querySelector('.desktop-menu');
+let menuMovil=document.querySelector('.menu');
+let elMEnu=document.querySelector('.mobile-menu');
+//escuchadores de eventos menu
+email.addEventListener('click',myCuenta);
+menuMovil.addEventListener('click',movilMenu)
+
+
+//funciones
 function myCuenta()
 {
     barra.classList.toggle('cambio')
 
+}
+function movilMenu()
+{
+    elMEnu.classList.toggle('cambio')
 }
